9bdfb68efe8fd1b5c5c2f6f82d4b6b43daf4e5e1
greeno-hope <60809887+greeno-hope@users.noreply.github.com>
2024-03-01 11:08:51 +0000

    First commit - template for SEGH coursework artifact

---
+++ src/main/java/com/example/seghrestapp/SeghRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.seghrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeghRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeghRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seghrestapp/SeghRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seghrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeghRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
